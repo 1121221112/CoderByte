45f0707c217eea2f73c5b8a563f7e4d4a9d89636
zhangping <15774560747@163.com>
2020-02-25 21:08:37 +0800

    字符变更简单算法

---
+++ LetterChanges.c
@@ -0,0 +1,34 @@
+#include <stdio.h> 
+#include <string.h>
+
+void LetterChanges(char str[]) {
+  
+  // code goes here  
+  char res[50];
+  memset(res,00,50);
+  for(int i=0;i<strlen(str);i++)
+  {
+    if(97<=str[i] && str[i]<=122)
+    {
+      res[i]=str[i]+1;
+    }      
+    else
+    {
+      res[i]=str[i];
+    }        
+    if(res[i]==97 || res[i]==101 || res[i]==105 || res[i]==111 || res[i]==117)
+    {
+      res[i]=res[i]-32;
+    }
+  }
+  printf("%s", res);
+
+}
+
+int main(void) { 
+   
+  // keep this function call here
+  LetterChanges(gets(stdin));
+  return 0;
+    
+}
